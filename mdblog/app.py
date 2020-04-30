dd62137f70afb0d44d63ee8c8a682bd44e1acc7e
pypepe <pe.pe@centrum.sk>
2020-04-30 20:08:14 +0000

    using jinja template system

--- mdblog/app.py
+++ mdblog/app.py
@@ -5,6 +5,7 @@
 
 from flask import Flask
 from flask import render_template
+from .database import articles
 
 flask_app = Flask(__name__)
 
@@ -23,4 +24,11 @@ def view_admin():
 
 @flask_app.route("/articles/")
 def view_articles():
-    return render_template("articles.jinja")
+    return render_template("articles.jinja", articles=articles.items())
+
+@flask_app.route("/articles/<int:art_id>")
+def view_article(art_id):
+    article = articles.get(art_id)
+    if article:
+        return render_template("article.jinja", article=article)
+    return render_template("article_not_found.jinja", art_id=art_id)
---
+++ mdblog/database.py
@@ -0,0 +1,18 @@
+articles = {
+    1 : {
+    "title" : "Hello World",
+    "content" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A neque excepturi officia quaerat in corporis ad atque illum, quasi, animi, quia nostrum aspernatur inventore, repudiandae sit praesentium amet eveniet illo?"
+    },
+    2 : {
+    "title" : "Hello World",
+    "content" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A neque excepturi officia quaerat in corporis ad atque illum, quasi, animi, quia nostrum aspernatur inventore, repudiandae sit praesentium amet eveniet illo?"
+    },
+    3 : {
+    "title" : "Hello World",
+    "content" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A neque excepturi officia quaerat in corporis ad atque illum, quasi, animi, quia nostrum aspernatur inventore, repudiandae sit praesentium amet eveniet illo?"
+    },
+    4 : {
+    "title" : "Hello World",
+    "content" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A neque excepturi officia quaerat in corporis ad atque illum, quasi, animi, quia nostrum aspernatur inventore, repudiandae sit praesentium amet eveniet illo?"
+    }
+}
